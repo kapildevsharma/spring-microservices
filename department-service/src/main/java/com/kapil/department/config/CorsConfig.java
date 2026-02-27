54cd453bfd186be68e536ccd3514e383d49e8c6c
kapilsharma05 <kapilgyanvihar@gmail.com>
2026-02-27 12:31:47 +0530

    Enhance functionality

--- department-service/src/main/java/com/kapil/department/config/CorsConfig.java
+++ department-service/src/main/java/com/kapil/department/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.kapil.department.config.config;
+package com.kapil.department.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- user-service/src/main/java/com/kapil/user/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/kapil/user/exception/GlobalExceptionHandler.java
@@ -3,13 +3,13 @@ package com.kapil.user.exception;
 import com.kapil.user.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Collections;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
